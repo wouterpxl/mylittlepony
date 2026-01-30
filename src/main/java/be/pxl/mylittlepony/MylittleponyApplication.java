f2379b60da6f3559b0dc5e2ae7597a303c12a37a
wouterg <wouter@brainbaking.com>
2026-01-30 11:14:02 +0100

    first commit

---
+++ src/main/java/be/pxl/mylittlepony/MylittleponyApplication.java
@@ -0,0 +1,13 @@
+package be.pxl.mylittlepony;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MylittleponyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MylittleponyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/pxl/mylittlepony/MylittleponyApplicationTests.java
@@ -0,0 +1,13 @@
+package be.pxl.mylittlepony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MylittleponyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
