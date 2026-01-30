e661e80b7d03dd4ff5d1a6dcb427bbfa7d1d3b43
wouterpxl <wouter@brainbaking.com>
2026-01-30 14:22:04 +0100

    making stuff worse (on purpose I promise)

--- src/main/java/be/pxl/mylittlepony/MyLittleController.java
+++ src/main/java/be/pxl/mylittlepony/MyLittleController.java
@@ -9,6 +9,7 @@ public class MyLittleController {
     @GetMapping("/hoi")
     public String hoi() {
         someNastyStuff(111);
+        moreShizzle(2);
         return "dfsdf";
     }
 
@@ -24,5 +25,30 @@ public class MyLittleController {
                 }
             }
         }
+    }
+
+    private void moreShizzle(int n) {
+        if (n > 1) {
+        }
+        if (n > 2) {
+        }
+        if (n > 3) {
+        }
+        if (n > 4) {
+        }
+        if (n > 5) {
+        }
+        if (n > 6) {
+        }
+        if (n > 7) {
+        }
+        if (n > 8) {
+        }
+        if (n > 9) {
+        }
+        if (n > 10) {
+        }
+        System.out.println("lol");
+
     }
 }
