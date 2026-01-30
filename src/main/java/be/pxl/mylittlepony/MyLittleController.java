969cd3000371725fcd2f235bf0b1beeead2e15c1
wouterpxl <wouter@brainbaking.com>
2026-01-30 13:43:24 +0100

    nasty stuff example

--- src/main/java/be/pxl/mylittlepony/MyLittleController.java
+++ src/main/java/be/pxl/mylittlepony/MyLittleController.java
@@ -8,7 +8,21 @@ public class MyLittleController {
 
     @GetMapping("/hoi")
     public String hoi() {
+        someNastyStuff(111);
         return "dfsdf";
     }
-}
 
+    private void someNastyStuff(int n) {
+        if (true) {
+            if (!false) {
+                for (int i = 0; i < 1000; i++) {
+                    if (true) {
+                        if (n > 10) {
+                            System.out.println("hallokes");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
