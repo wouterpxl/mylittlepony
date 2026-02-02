430c3f8e859284e10d6ee0b9fffc79f1333e894d
wouterpxl <wouter@brainbaking.com>
2026-02-02 10:38:06 +0100

    frontend: lint test

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,5 +1,14 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+
+
+function something() {
+   if(true) {
+     if(!false) {
+       console.log("I'm creating a big mess here");
+     }
+   }
+ }
 </script>
 
 <template>
